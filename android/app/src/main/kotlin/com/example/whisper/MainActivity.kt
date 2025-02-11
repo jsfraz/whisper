7b99b34e501543ecac360a047a711c19c2e7b773
jsfraz <josefraz@seznam.cz>
2025-02-11 20:34:58 +0100

    Disabled screenshotting.

--- android/app/src/main/kotlin/com/example/whisper/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/whisper/MainActivity.kt
@@ -1,5 +1,14 @@
 package cz.josefraz.whisper
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+// https://medium.com/@sharansukesh2000/protecting-your-flutter-app-implementing-screenshot-prevention-3c06d028e682
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // below line prevents the user from taking screenshot or record the screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+    }
+}
